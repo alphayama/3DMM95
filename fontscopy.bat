1cf2a1bafd2ddbf9fd2a31d36d457d8ee6c33e11
Ashish Pratap <26324100+alphayama@users.noreply.github.com>
2022-05-05 16:40:27 -0500

    add Font copying batch file

---
+++ fontscopy.bat
@@ -0,0 +1,6 @@
+copy C:\Windows\Fonts\COMICBD.TTF .\cd3\SETUP\
+copy C:\Windows\Fonts\COMIC.TTF .\cd3\SETUP\
+copy C:\Windows\Fonts\COMICBD.TTF .\SETUP\BIN\
+copy C:\Windows\Fonts\COMIC.TTF .\SETUP\BIN\
+copy C:\Windows\Fonts\COMICBD.TTF .\cd12\SETUP\BIN\
+copy C:\Windows\Fonts\COMIC.TTF .\cd12\SETUP\BIN\
